da1f6f9aba11b7af665fac68d0ede47701a19784
qhuydtvt <qhuydtvt@gmail.com>
2016-10-28 23:40:33 +0700

    Changed to flask-admin

--- app.py
+++ app.py
@@ -1,50 +1,133 @@
-from flask import *
-import flask_login
-from flask_login import *
+import datetime
 
-from models.mlab import *
-from models.portfolio import *
-from models.users import *
+from flask import Flask
 
+import flask_admin as admin
+from flask_mongoengine import MongoEngine
+from flask_admin.form import rules
+from flask_admin.contrib.mongoengine import ModelView
+
+# Create application
 app = Flask(__name__)
-app.secret_key = "fD226QUKwZ5yta8yzFhpnmEdIfsbvmXjTc2qwkOn"
-login_manager = LoginManager()
-login_manager.init_app(app)
-mlab_connect()
 
+# Create dummy secrey key so we can use sessions
+app.config['SECRET_KEY'] = '123456790'
+app.config['MONGODB_SETTINGS'] = {'DB': 'testing'}
+
+# Create models
+db = MongoEngine()
+db.init_app(app)
+
+
+# Define mongoengine documents
+class User(db.Document):
+    name = db.StringField(max_length=40)
+    tags = db.ListField(db.ReferenceField('Tag'))
+    password = db.StringField(max_length=40)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Todo(db.Document):
+    title = db.StringField(max_length=60)
+    text = db.StringField()
+    done = db.BooleanField(default=False)
+    pub_date = db.DateTimeField(default=datetime.datetime.now)
+    user = db.ReferenceField(User, required=False)
+
+    # Required for administrative interface
+    def __unicode__(self):
+        return self.title
+
+
+class Tag(db.Document):
+    name = db.StringField(max_length=10)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Comment(db.EmbeddedDocument):
+    name = db.StringField(max_length=20, required=True)
+    value = db.StringField(max_length=20)
+    tag = db.ReferenceField(Tag)
+
+
+class Post(db.Document):
+    name = db.StringField(max_length=20, required=True)
+    value = db.StringField(max_length=20)
+    inner = db.ListField(db.EmbeddedDocumentField(Comment))
+    lols = db.ListField(db.StringField(max_length=20))
+
+
+class File(db.Document):
+    name = db.StringField(max_length=20)
+    data = db.FileField()
+
+
+class Image(db.Document):
+    name = db.StringField(max_length=20)
+    image = db.ImageField(thumbnail_size=(100, 100, True))
+
+
+# Customized admin views
+class UserView(ModelView):
+    column_filters = ['name']
+
+    column_searchable_list = ('name', 'password')
+
+    form_ajax_refs = {
+        'tags': {
+            'fields': ('name',)
+        }
+    }
+
+
+class TodoView(ModelView):
+    column_filters = ['done']
+
+    form_ajax_refs = {
+        'user': {
+            'fields': ['name']
+        }
+    }
+
+
+class PostView(ModelView):
+    form_subdocuments = {
+        'inner': {
+            'form_subdocuments': {
+                None: {
+                    # Add <hr> at the end of the form
+                    'form_rules': ('name', 'tag', 'value', rules.HTML('<hr>')),
+                    'form_widget_args': {
+                        'name': {
+                            'style': 'color: red'
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+# Flask views
 @app.route('/')
 def index():
-    return render_template("index.html", porfolios=Portfolio.objects)
+    return '<a href="/admin/">Click me to get to Admin!</a>'
 
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    if request.method == 'GET':
-        return render_template("login.html")
-    elif request.method == 'POST':
-        form = request.form
-        email = form["email"]
-        password = form["password"]
-        user_login = UserLogin.check(email, password)
-        if user_login:
-            flask_login.login_user(user_login)
-            return redirect(url_for("admin"))
-        return "Invalid credentials"
-
-@app.route('/logout', methods=["GET", "POST"])
-def logout():
-    flask_login.logout_user()
-    return redirect(url_for("index"))
-
-@app.route('/admin')
-@flask_login.login_required
-def admin():
-    return "admin"
-
-@login_manager.user_loader
-def user_loader(email):
-    return UserLogin.get(email)
 
 if __name__ == '__main__':
-    app.run()
+    # Create admin
+    admin = admin.Admin(app, 'Example: MongoEngine')
 
+    # Add views
+    admin.add_view(UserView(User))
+    admin.add_view(TodoView(Todo))
+    admin.add_view(ModelView(Tag))
+    admin.add_view(PostView(Post))
+    admin.add_view(ModelView(File))
+    admin.add_view(ModelView(Image))
 
+    # Start app
+    app.run(debug=True)
